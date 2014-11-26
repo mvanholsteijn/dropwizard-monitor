f35dae24345e9507a925d1a7744cfe9316548e6a
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2014-11-26 17:00:54 +0100

    first import

---
+++ src/main/java/com/xebia/drop/helloworld/resource/HelloWorldResource.java
@@ -0,0 +1,34 @@
+package com.xebia.drop.helloworld.resource;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.google.common.base.Optional;
+import com.xebia.drop.helloworld.core.Saying;
+import com.yammer.metrics.annotation.Timed;
+
+@Path("/hello-world")
+@Produces(MediaType.APPLICATION_JSON)
+public class HelloWorldResource {
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+
+    public HelloWorldResource(String template, String defaultName) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = new AtomicLong();
+    }
+
+    @GET
+    @Timed
+    public Saying sayHello(@QueryParam("name") Optional<String> name) {
+        return new Saying(counter.incrementAndGet(),
+                          String.format(template, name.or(defaultName)));
+    }
+}
